53b6925011c3586446c0f9b5160c974d52570c41
viktorvlasuk <viktr.vlasjuk1@yandex.ru>
2026-04-22 13:33:42 +0300

    страница теперь стала лучше авторизации там больше информации и представления для пользователя

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,6 +3,4 @@ export { SearchPage } from './SearchPage';
 export { RecipePage } from './RecipePage';
 export { CookPage } from './CookPage';
 export { ProfilePage } from './ProfilePage';
-
-// Auth page остаётся в pages/AuthPage.tsx — её реэкспорт тоже нужен
 export { AuthPage } from './AuthPage';
